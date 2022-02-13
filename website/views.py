0e41c8d59707153e786869328b48391f109ee6cb
yehoon17 <yehoon17@gmail.com>
2022-02-13 16:58:06 +0900

    Update views.py
    Get currency cost from poe.ninja

--- website/views.py
+++ website/views.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint, render_template
+import requests
 
+CURRENCY_URL = "https://poe.ninja/api/data/currencyoverview?league=Archnemesis&type=Currency"
 
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
 def home():
-    return render_template("home.html")
+    response = requests.get(CURRENCY_URL)
+    content = response.json()["lines"]
+    currencies = ["Vaal Orb", "Prime Regrading Lens", "Secondary Regrading Lens"]
+    filtered = filter(lambda x: x["currencyTypeName"] in currencies, content)
+    costs = list(map(lambda x: (x["currencyTypeName"], x["chaosEquivalent"]), filtered))
+    
+    return render_template("home.html", costs=costs)
