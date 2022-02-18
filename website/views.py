2b1895021fe4484dc6900576d8778fa1fb57eca0
yehoon17 <yehoon17@gmail.com>
2022-02-18 14:57:56 +0900

    Update views.py

--- website/views.py
+++ website/views.py
@@ -2,18 +2,28 @@
 
 from flask import Blueprint, render_template
 import requests
+import pandas as pd
 
 CURRENCY_URL = "https://poe.ninja/api/data/currencyoverview?league=Archnemesis&type=Currency"
+SKILL_GEM_URL = "https://poe.ninja/api/data/itemoverview?league=Archnemesis&type=SkillGem"
 
 views = Blueprint("views", __name__)
 
 
-@views.route("/")
-def home():
+def get_currency():
     response = requests.get(CURRENCY_URL)
     content = response.json()["lines"]
-    currencies = ["Vaal Orb", "Prime Regrading Lens", "Secondary Regrading Lens"]
+    currencies = ["Vaal Orb", "Prime Regrading Lens",
+                  "Secondary Regrading Lens"]
     filtered = filter(lambda x: x["currencyTypeName"] in currencies, content)
-    costs = list(map(lambda x: (x["currencyTypeName"], x["chaosEquivalent"]), filtered))
-    
+    return list(map(lambda x: (x["currencyTypeName"], x["chaosEquivalent"]), filtered))
+
+
+def get_gem_sample():
+    pass
+
+
+@views.route("/")
+def home():
+    costs = get_currency()
     return render_template("home.html", costs=costs)
