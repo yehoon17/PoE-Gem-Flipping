accb4ba2ff0fd52f072c57787b35058c1cef6b7c
yehoon17 <yehoon17@gmail.com>
2022-02-20 10:17:32 +0900

    Update models.py
    Create Currency and Gem

--- website/models.py
+++ website/models.py
@@ -2,3 +2,17 @@
 
 from . import db
 
+
+class Currency(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    value = db.Column(db.Float, nullable=False)
+
+
+class Gem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    corrupted = db.Column(db.Boolean, nullable=False)
+    level = db.Column(db.Integer, nullable=False)
+    quality = db.Column(db.Integer, nullable=False)
+    value = db.Column(db.Float, nullable=False)
+    listed = db.Column(db.Integer, nullable=False)
