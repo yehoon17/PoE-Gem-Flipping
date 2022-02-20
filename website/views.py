d1869186783e48642ba63e53dc383ba8d2808ed0
yehoon17 <yehoon17@gmail.com>
2022-02-20 10:18:06 +0900

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint, render_template, request
-import requests
-import pandas as pd
-
-CURRENCY_URL = "https://poe.ninja/api/data/currencyoverview?league=Archnemesis&type=Currency"
-SKILL_GEM_URL = "https://poe.ninja/api/data/itemoverview?league=Archnemesis&type=SkillGem"
+from . import db
+from website.utils import *
 
 views = Blueprint("views", __name__)
-
-
-def get_currency():
-    response = requests.get(CURRENCY_URL)
-    content = response.json()["lines"]
-    currencies = ["Vaal Orb", "Prime Regrading Lens",
-                  "Secondary Regrading Lens"]
-    filtered = filter(lambda x: x["currencyTypeName"] in currencies, content)
-    return list(map(lambda x: (x["currencyTypeName"], x["chaosEquivalent"]), filtered))
-
-
-def get_gem_sample():
-    pass
 
 
 @views.route("/")
@@ -35,5 +19,5 @@ def flipping_method():
     ### query gems of flipping-method from database ###
     gems = None
     ###################################################
-    
+
     return render_template("home.html", gems=gems)
