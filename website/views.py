a52f0daa8d8ac3f9942be49509578fd375f7fc37
yehoon17 <yehoon17@gmail.com>
2022-02-21 14:51:11 +0900

    Update views.py
    Pass currency to html

--- website/views.py
+++ website/views.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from locale import currency
 from flask import Blueprint, render_template, request
 from . import db
 from website.utils import *
+from website.models import Currency
 
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
 def home():
-    return render_template("home.html")
+    update_currency()
+    currency = Currency.query.all()
+    return render_template("home.html", currency=currency)
 
 
 @views.route("/flipping/")
