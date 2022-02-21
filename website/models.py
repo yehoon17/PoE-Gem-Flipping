eb27c44c3191b192608821bfc430f0a9524b3535
yehoon17 <yehoon17@gmail.com>
2022-02-22 03:56:05 +0900

    Update models.py

--- website/models.py
+++ website/models.py
@@ -11,8 +11,17 @@ class Currency(db.Model):
 
 class Gem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
     corrupted = db.Column(db.Boolean, nullable=False)
     level = db.Column(db.Integer, nullable=False)
     quality = db.Column(db.Integer, nullable=False)
     value = db.Column(db.Float, nullable=False)
     listed = db.Column(db.Integer, nullable=False)
+
+
+class GemInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    has_vaal = db.Column(db.Boolean, nullable=False)
+    max_level = db.Column(db.Integer, nullable=False)
+    is_skill = db.Column(db.Boolean, nullable=False)
